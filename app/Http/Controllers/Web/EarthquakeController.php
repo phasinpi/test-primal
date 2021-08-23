8503266de714b47b7485498c36a30d4c50dd43b8
Phasin Piamviriya <phasin@shareinvestor.com>
2021-08-23 09:26:07 +0700

    clean code

--- app/Http/Controllers/Web/EarthquakeController.php
+++ app/Http/Controllers/Web/EarthquakeController.php
@@ -15,16 +15,6 @@ class EarthquakeController extends Controller
      */
     public function index()
     {
-        $client = new \GuzzleHttp\Client();
-        $request = $client->get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
-        // $response = $request->getBody();
-        $response = json_decode($request->getBody()->getContents(), true);
-        // print_r($response);
-        // print_r(json_encode($response));
-        // return $response['features'];
-        // foreach($response['features'] as $feature) {
-        //     return $feature['geometry']['coordinates'][0];
-        // }
-        return view('earthquake.index', ['response' => $response]);
+        return view('earthquake.index');
     }
 }
--- resources/views/earthquake/index.blade.php
+++ resources/views/earthquake/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<style type="text/css">
+<style type="text/css"> //style map
     #mymap {
         border: 1px solid red;
         width: 100%;
@@ -13,54 +13,31 @@
 <div class="container">
     <div class="row mb-5">
         <div class="col-12">
-            <div id="mymap"></div>
+            <div id="mymap"></div> //google map
         </div>
     </div>
 </div>
 
 <script type="text/javascript">
-    var mymap = new GMaps({
+    var mymap = new GMaps({ // setmap center
       el: '#mymap',
       lat: 16.542746235328945,
       lng: 0.006936576886640835,
       zoom:2
     });
 
-    $.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(data){
+    $.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(data){ //cal api earthquake
         var locations = data.features;
-        $.each( locations, function( index, value ){
-            console.log(value.geometry.coordinates[0]);
-            mymap.addMarker({
+        $.each( locations, function( index, value ){ //loop location
+            mymap.addMarker({ //marker pin location
                 lat: value.geometry.coordinates[0],
                 lng: value.geometry.coordinates[0],
                 title: value.properties.title,
-                click: function(e) {
+                click: function(e) { //popup location name when click
                   alert('This is '+value.properties.place);
                 }
             });
         });
     });
-    {{-- var locations = {{$response['features']}};
-    console.log(locations); --}}
-
-    {{-- var mymap = new GMaps({
-      el: '#mymap',
-      lat: 21.170240,
-      lng: 72.831061,
-      zoom:6
-    }); --}}
-
-{{--
-    $.each( locations, function( index, value ){
-        console.log(value['geometry']['coordinates'][0]);
-	    mymap.addMarker({
-	      lat: value['geometry']['coordinates'][0],
-	      lng: value.lng,
-	      title: value.city,
-	      click: function(e) {
-	        alert('This is '+value.city+', gujarat from India.');
-	      }
-	    });
-   }); --}}
 </script>
 @endsection
